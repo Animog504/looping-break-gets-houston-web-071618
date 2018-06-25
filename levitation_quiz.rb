a5448855cddc21cfb83f840eb1cd90a3ec25550f
Scott Ungchusri <animoogle504@gmail.com>
2018-06-25 21:53:13 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,10 +2,12 @@
 def levitation_quiz
 	#your code here
 	loop do
-	gets answer
-	if answer = "Wingardium Leviosa"
+	  
+	answer = gets.chomp
+	if answer == "Wingardium Leviosa"
 	  break
 	end
+	
 end
 
 
